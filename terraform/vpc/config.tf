c7d28dd113009de6d40c346cf60a25212894e6a4
YohanP <yohan.peyen@d2-si.eu>
2018-07-19 11:13:58 +0200

    format

--- terraform/vpc/config.tf
+++ terraform/vpc/config.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "ype-demo-veolia"
-    key    = "vpc/vpc.state"
-    region = "eu-west-1"
+    bucket         = "ype-demo-veolia"
+    key            = "vpc/vpc.state"
+    region         = "eu-west-1"
     dynamodb_table = "demo-veolia"
   }
 }
-
--- terraform/vpc/variables.tf
+++ terraform/vpc/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {}
 
 variable azs {
-    default = {
-        "eu-west-1" = "eu-west-1a,eu-west-1b"
-        "us-west-2" = "us-west-2a,us-west-2b"
-        "us-east-1" = "us-east-1a,us-east-1b"
-    }
+  default = {
+    "eu-west-1" = "eu-west-1a,eu-west-1b"
+    "us-west-2" = "us-west-2a,us-west-2b"
+    "us-east-1" = "us-east-1a,us-east-1b"
+  }
 }
 
 variable "vpc_name" {}
